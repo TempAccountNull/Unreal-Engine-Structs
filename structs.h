c7c04cd0d51db640687d6b77cd899757b3faa170
hambabler <91430598+hambabler@users.noreply.github.com>
2022-03-24 15:10:54 -0400

    Create structs.h

---
+++ structs.h
@@ -0,0 +1,556 @@
+#pragma once
+
+#include <windows.h>
+#include <psapi.h>
+#include <tlhelp32.h>
+#include <iostream>
+#include <dwmapi.h>
+#include <d3d9.h>
+#include <string>
+#include <map>
+
+// TArray
+template<class T>
+class TArray
+{
+public:
+	int Length() const
+	{
+		return m_nCount;
+	}
+
+	bool IsValid() const
+	{
+		if (m_nCount > m_nMax)
+			return false;
+		if (!m_Data)
+			return false;
+		return true;
+	}
+
+	uint64_t GetAddress() const
+	{
+		return m_Data;
+	}
+
+	T GetById(int i)
+	{
+		return read<T>(m_Data + i * 8);
+	}
+
+protected:
+	uint64_t m_Data;
+	uint32_t m_nCount;
+	uint32_t m_nMax;
+};
+
+// Positional/Coordinate Classes & Structs
+
+#define M_PI 3.14159265358979323846264338327950288419716939937510
+class Vector3
+{
+public:
+	Vector3() : x(0.f), y(0.f), z(0.f)
+	{
+
+	}
+
+	Vector3(float _x, float _y, float _z) : x(_x), y(_y), z(_z)
+	{
+
+	}
+	~Vector3()
+	{
+
+	}
+
+	float x;
+	float y;
+	float z;
+
+	inline float Dot(Vector3 v)
+	{
+		return x * v.x + y * v.y + z * v.z;
+	}
+
+	inline float Distance(Vector3 v)
+	{
+		float x = this->x - v.x;
+		float y = this->y - v.y;
+		float z = this->z - v.z;
+
+		return sqrtf((x * x) + (y * y) + (z * z)) * 0.03048f;
+	}
+
+    inline double Length() {
+        return (double)(Customsqrtf_(x * x + y * y + z * z));
+    }
+
+	Vector3 operator+(Vector3 v)
+	{
+		return Vector3(x + v.x, y + v.y, z + v.z);
+	}
+
+	Vector3 operator-(Vector3 v)
+	{
+		return Vector3(x - v.x, y - v.y, z - v.z);
+	}
+
+	Vector3 operator*(float number) const {
+		return Vector3(x * number, y * number, z * number);
+	}
+};
+
+// FVector, used for worldspace coordinates/other stuff
+
+struct FVector
+{
+	float X;
+	float Y;
+	float Z;
+
+	inline FVector()
+		: X(0), Y(0), Z(0) {
+	}
+
+	inline FVector(float x, float y, float z) : X(x), Y(y), Z(z) {}
+
+	__forceinline FVector operator-(const FVector& V) {
+		return FVector(X - V.X, Y - V.Y, Z - V.Z);
+	}
+
+	inline float Distance(FVector v)
+	{
+		float x = this->X - v.X;
+		float y = this->Y - v.Y;
+		float z = this->Z - v.Z;
+
+		return sqrtf((x * x) + (y * y) + (z * z)) * 0.03048f;
+	}
+
+	__forceinline FVector operator+(const FVector& V) {
+		return FVector(X + V.X, Y + V.Y, Z + V.Z);
+	}
+
+	__forceinline FVector operator*(float Scale) const {
+		return FVector(X * Scale, Y * Scale, Z * Scale);
+	}
+
+	__forceinline FVector operator/(float Scale) const {
+		const float RScale = 1.f / Scale;
+		return FVector(X * RScale, Y * RScale, Z * RScale);
+	}
+
+	__forceinline FVector operator+(float A) const {
+		return FVector(X + A, Y + A, Z + A);
+	}
+
+	__forceinline FVector operator-(float A) const {
+		return FVector(X - A, Y - A, Z - A);
+	}
+
+	__forceinline FVector operator*(const FVector& V) const {
+		return FVector(X * V.X, Y * V.Y, Z * V.Z);
+	}
+
+	__forceinline FVector operator/(const FVector& V) const {
+		return FVector(X / V.X, Y / V.Y, Z / V.Z);
+	}
+
+	__forceinline float operator|(const FVector& V) const {
+		return X * V.X + Y * V.Y + Z * V.Z;
+	}
+
+	__forceinline float operator^(const FVector& V) const {
+		return X * V.Y - Y * V.X - Z * V.Z;
+	}
+
+	__forceinline FVector& operator+=(const FVector& v) {
+		X += v.X;
+		Y += v.Y;
+		Z += v.Z;
+		return *this;
+	}
+
+	__forceinline FVector& operator-=(const FVector& v) {
+		X -= v.X;
+		Y -= v.Y;
+		Z -= v.Z;
+		return *this;
+	}
+
+	__forceinline FVector& operator*=(const FVector& v) {
+		X *= v.X;
+		Y *= v.Y;
+		Z *= v.Z;
+		return *this;
+	}
+
+	__forceinline FVector& operator/=(const FVector& v) {
+		X /= v.X;
+		Y /= v.Y;
+		Z /= v.Z;
+		return *this;
+	}
+
+	__forceinline bool operator==(const FVector& src) const {
+		return (src.X == X) && (src.Y == Y) && (src.Z == Z);
+	}
+
+	__forceinline bool operator!=(const FVector& src) const {
+		return (src.X != X) || (src.Y != Y) || (src.Z != Z);
+	}
+
+	__forceinline float Size() const {
+		return sqrt(X * X + Y * Y + Z * Z);
+	}
+
+	__forceinline float Size2D() const {
+		return sqrt(X * X + Y * Y);
+	}
+
+	__forceinline float SizeSquared() const {
+		return X * X + Y * Y + Z * Z;
+	}
+
+	__forceinline float SizeSquared2D() const {
+		return X * X + Y * Y;
+	}
+
+	__forceinline float Dot(const FVector& vOther) const {
+		const FVector& a = *this;
+		return (a.X * vOther.X + a.Y * vOther.Y + a.Z * vOther.Z);
+	}
+
+	__forceinline FVector Normalize() {
+		FVector vector;
+		float length = this->Size();
+
+		if (length != 0) {
+			vector.X = X / length;
+			vector.Y = Y / length;
+			vector.Z = Z / length;
+		}
+		else {
+			vector.X = vector.Y = 0.0f;
+			vector.Z = 1.0f;
+		}
+		return vector;
+	}
+
+	__forceinline FVector ToRotator() {
+		FVector rotator;
+		rotator.X = -(float)atan(Z / Size2D()) * (float)(180.0f / M_PI);
+		rotator.Y = (float)atan(Y / X) * (float)(180.0f / M_PI);
+		rotator.Z = (float)0.f;
+		if (X >= 0.f)
+			rotator.Y += 180.0f;
+		return rotator;
+	}
+
+	__forceinline FVector Clamp()
+	{
+		FVector clamped;
+		while (clamped.Y < -180.0f)
+			clamped.Y += 360.0f;
+		while (clamped.Y > 180.0f)
+			clamped.Y -= 360.0f;
+		if (clamped.X < -74.0f)
+			clamped.X = -74.0f;
+		if (clamped.X > 74.0f)
+			clamped.X = 74.0f;
+		return clamped;
+	}
+};
+
+struct FQuat
+{
+	float x;
+	float y;
+	float z;
+	float w;
+};
+
+struct FTransform
+{
+	FQuat rot;
+	Vector3 translation;
+	char pad[4];
+	Vector3 scale;
+	char pad1[4];
+
+	D3DMATRIX ToMatrixWithScale()
+	{
+		D3DMATRIX m;
+		m._41 = translation.x;
+		m._42 = translation.y;
+		m._43 = translation.z;
+
+		float x2 = rot.x + rot.x;
+		float y2 = rot.y + rot.y;
+		float z2 = rot.z + rot.z;
+
+		float xx2 = rot.x * x2;
+		float yy2 = rot.y * y2;
+		float zz2 = rot.z * z2;
+		m._11 = (1.0f - (yy2 + zz2)) * scale.x;
+		m._22 = (1.0f - (xx2 + zz2)) * scale.y;
+		m._33 = (1.0f - (xx2 + yy2)) * scale.z;
+
+		float yz2 = rot.y * z2;
+		float wx2 = rot.w * x2;
+		m._32 = (yz2 - wx2) * scale.z;
+		m._23 = (yz2 + wx2) * scale.y;
+
+		float xy2 = rot.x * y2;
+		float wz2 = rot.w * z2;
+		m._21 = (xy2 - wz2) * scale.y;
+		m._12 = (xy2 + wz2) * scale.x;
+
+		float xz2 = rot.x * z2;
+		float wy2 = rot.w * y2;
+		m._31 = (xz2 + wy2) * scale.z;
+		m._13 = (xz2 - wy2) * scale.x;
+
+		m._14 = 0.0f;
+		m._24 = 0.0f;
+		m._34 = 0.0f;
+		m._44 = 1.0f;
+
+		return m;
+	}
+};
+
+// FString, sometimes used to store Player Names, etc.
+
+struct FString : private TArray<wchar_t>
+{
+	std::wstring ToWString() const
+	{
+		wchar_t* buffer = new wchar_t[m_nCount];
+		read_array(m_Data, buffer, m_nCount);
+		std::wstring ws(buffer);
+		delete[] buffer;
+
+		return ws;
+	}
+
+	std::string ToString() const
+	{
+		std::wstring ws = ToWString();
+		std::string str(ws.begin(), ws.end());
+
+		return str;
+	}
+};
+
+std::string GetNameFromFName(int key)
+{
+	auto chunkOffset = (UINT)((int)(key) >> 16);
+	auto nameOffset = (USHORT)key;
+
+	auto namePoolChunk = read<UINT64>(ProcessBase + GNames + ((chunkOffset + 2) * 8));
+	auto entryOffset = namePoolChunk + (ULONG)(2 * nameOffset);
+	auto nameEntry = read<INT16>(entryOffset);
+
+	auto nameLength = nameEntry >> 6;
+	char buff[1028];
+	if ((DWORD)nameLength && nameLength > 0)
+	{
+		read_array(entryOffset + 2, buff, nameLength);
+		buff[nameLength] = '\0';
+		return std::string(buff);
+	}
+	else return "";
+}
+
+// Matrix
+
+D3DXMATRIX Matrix(Vector3 rot, Vector3 origin)
+{
+	float radPitch = (rot.x * float(M_PI) / 180.f);
+	float radYaw = (rot.y * float(M_PI) / 180.f);
+	float radRoll = (rot.z * float(M_PI) / 180.f);
+
+	float SP = sinf(radPitch);
+	float CP = cosf(radPitch);
+	float SY = sinf(radYaw);
+	float CY = cosf(radYaw);
+	float SR = sinf(radRoll);
+	float CR = cosf(radRoll);
+
+	D3DMATRIX matrix;
+	matrix.m[0][0] = CP * CY;
+	matrix.m[0][1] = CP * SY;
+	matrix.m[0][2] = SP;
+	matrix.m[0][3] = 0.f;
+
+	matrix.m[1][0] = SR * SP * CY - CR * SY;
+	matrix.m[1][1] = SR * SP * SY + CR * CY;
+	matrix.m[1][2] = -SR * CP;
+	matrix.m[1][3] = 0.f;
+
+	matrix.m[2][0] = -(CR * SP * CY + SR * SY);
+	matrix.m[2][1] = CY * SR - CR * SP * SY;
+	matrix.m[2][2] = CR * CP;
+	matrix.m[2][3] = 0.f;
+
+	matrix.m[3][0] = origin.x;
+	matrix.m[3][1] = origin.y;
+	matrix.m[3][2] = origin.z;
+	matrix.m[3][3] = 1.f;
+
+	return matrix;
+}
+
+D3DMATRIX MatrixMultiplication(D3DMATRIX pM1, D3DMATRIX pM2)
+{
+	D3DMATRIX pOut;
+	pOut._11 = pM1._11 * pM2._11 + pM1._12 * pM2._21 + pM1._13 * pM2._31 + pM1._14 * pM2._41;
+	pOut._12 = pM1._11 * pM2._12 + pM1._12 * pM2._22 + pM1._13 * pM2._32 + pM1._14 * pM2._42;
+	pOut._13 = pM1._11 * pM2._13 + pM1._12 * pM2._23 + pM1._13 * pM2._33 + pM1._14 * pM2._43;
+	pOut._14 = pM1._11 * pM2._14 + pM1._12 * pM2._24 + pM1._13 * pM2._34 + pM1._14 * pM2._44;
+	pOut._21 = pM1._21 * pM2._11 + pM1._22 * pM2._21 + pM1._23 * pM2._31 + pM1._24 * pM2._41;
+	pOut._22 = pM1._21 * pM2._12 + pM1._22 * pM2._22 + pM1._23 * pM2._32 + pM1._24 * pM2._42;
+	pOut._23 = pM1._21 * pM2._13 + pM1._22 * pM2._23 + pM1._23 * pM2._33 + pM1._24 * pM2._43;
+	pOut._24 = pM1._21 * pM2._14 + pM1._22 * pM2._24 + pM1._23 * pM2._34 + pM1._24 * pM2._44;
+	pOut._31 = pM1._31 * pM2._11 + pM1._32 * pM2._21 + pM1._33 * pM2._31 + pM1._34 * pM2._41;
+	pOut._32 = pM1._31 * pM2._12 + pM1._32 * pM2._22 + pM1._33 * pM2._32 + pM1._34 * pM2._42;
+	pOut._33 = pM1._31 * pM2._13 + pM1._32 * pM2._23 + pM1._33 * pM2._33 + pM1._34 * pM2._43;
+	pOut._34 = pM1._31 * pM2._14 + pM1._32 * pM2._24 + pM1._33 * pM2._34 + pM1._34 * pM2._44;
+	pOut._41 = pM1._41 * pM2._11 + pM1._42 * pM2._21 + pM1._43 * pM2._31 + pM1._44 * pM2._41;
+	pOut._42 = pM1._41 * pM2._12 + pM1._42 * pM2._22 + pM1._43 * pM2._32 + pM1._44 * pM2._42;
+	pOut._43 = pM1._41 * pM2._13 + pM1._42 * pM2._23 + pM1._43 * pM2._33 + pM1._44 * pM2._43;
+	pOut._44 = pM1._41 * pM2._14 + pM1._42 * pM2._24 + pM1._43 * pM2._34 + pM1._44 * pM2._44;
+
+	return pOut;
+}
+
+// Functions for getting bones (maybe player skeleton), and checking if mesh is visible or not
+
+FTransform GetBoneIndex(DWORD_PTR mesh, int index)
+{
+	DWORD_PTR bonearray = read<DWORD_PTR>(mesh + BoneArrayOffset);
+	if (!bonearray)
+		bonearray = read<DWORD_PTR>(mesh + BoneArrayOffset + 0x10);
+	return read<FTransform>(bonearray + (index * 0x30));
+}
+
+bool isVisible(DWORD_PTR mesh)
+{
+	float fLastSubmitTime = read<float>(mesh + LastSubmitTimeOffset);
+	float fLastRenderTimeOnScreen = read<float>(mesh + LastRenderTimeOffset);
+	const float fVisionTick = 0.06f;
+	bool bVisible = fLastRenderTimeOnScreen + fVisionTick >= fLastSubmitTime;
+	return bVisible;
+}
+
+Vector3 GetBoneWithRotation(DWORD_PTR mesh, int id)
+{
+	FTransform bone = GetBoneIndex(mesh, id);
+	FTransform ComponentToWorld = read<FTransform>(mesh + ComponentToWorldOffset);
+	D3DMATRIX Matrix;
+	Matrix = MatrixMultiplication(bone.ToMatrixWithScale(), ComponentToWorldOffset.ToMatrixWithScale());
+	return Vector3(Matrix._41, Matrix._42, Matrix._43);
+}
+
+// FLinearColor, used for well.. colors
+
+struct FLinearColor
+{
+	float R;
+	float G;
+	float B;
+	float A;
+};
+
+// FRotator, can use tis for aimbot
+
+struct FRotator
+{
+public:
+	int Pitch;
+	int Yaw;
+	int Roll;
+
+	inline FRotator operator+(FRotator a) {
+		return { a.Pitch + Pitch,a.Yaw + Yaw, a.Roll + Roll };
+	}
+
+	inline FRotator operator-(FRotator a) {
+		return { a.Pitch - Pitch,a.Yaw - Yaw, a.Roll - Roll };
+	}
+
+	inline FRotator operator*(FRotator a) {
+		return { a.Pitch * Pitch,a.Yaw * Yaw, a.Roll * Roll };
+	}
+
+	inline FRotator operator*(float a) {
+		return { (int)round(a * Pitch),(int)round(a * Yaw), (int)round(a * Roll) };
+	}
+};
+
+// Players hitboxes :O
+
+struct FBoxSphereBounds
+{
+	FVector Origin;
+	FVector BoxExtent;
+	float SphereRadius;
+};
+
+// Camera Stuff
+
+struct FMinimalViewInfo
+{
+	Vector3 Location;
+	Vector3 Rotation;
+	float FOV;
+	float DesiredFOV;
+	float OrthoWidth;
+	float OrthoNearClipPlane;
+	float OrthoFarClipPlane;
+	float AspectRatio;
+};
+
+struct FCameraCacheEntry
+{
+	float Timestamp;
+	char pad_4[0xc];
+	FMinimalViewInfo POV;
+};
+
+// Sample W2S, you will need to edit this. Just an example
+
+Vector3 W2S(Vector3 WorldLocation)
+{
+	Vector3 Screenlocation = Vector3(0, 0, 0);
+
+	auto CameraManager = read<uintptr_t>(LocalControllerOffset + CameraManagerOffset);
+	auto CameraCache = read<FCameraCacheEntry>(CameraManager + CameraCacheOffset);
+
+	D3DMATRIX tempMatrix = Matrix(Vector3(CameraCache.POV.Rotation), Vector3(0, 0, 0));
+
+	Vector3 vAxisX, vAxisY, vAxisZ;
+
+	vAxisX = Vector3(tempMatrix.m[0][0], tempMatrix.m[0][1], tempMatrix.m[0][2]);
+	vAxisY = Vector3(tempMatrix.m[1][0], tempMatrix.m[1][1], tempMatrix.m[1][2]);
+	vAxisZ = Vector3(tempMatrix.m[2][0], tempMatrix.m[2][1], tempMatrix.m[2][2]);
+
+	Vector3 vDelta = WorldLocation - Vector3(CameraCache.POV.Location);
+	Vector3 vTransformed = Vector3(vDelta.Dot(vAxisY), vDelta.Dot(vAxisZ), vDelta.Dot(vAxisX));
+
+	if (vTransformed.z < 1.f)
+		vTransformed.z = 1.f;
+
+	float RadFOV = (float)(ScreenCenterY / tan(0.0174532925 * (CameraCache.POV.FOV * 0.5f)));
+	float FovAngle = (float)(2 * 57.2957795 * (atanf(ScreenCenterX / RadFOV)));
+
+	float ScreenCenterX = ScreenWidth / 2.0f;
+	float ScreenCenterY = ScreenHeight / 2.0f;
+
+	Screenlocation.x = ScreenCenterX + vTransformed.x * (ScreenCenterX / tanf(FovAngle * (float)M_PI / 360.f)) / vTransformed.z;
+	Screenlocation.y = ScreenCenterY - vTransformed.y * (ScreenCenterX / tanf(FovAngle * (float)M_PI / 360.f)) / vTransformed.z;
+
+	return Screenlocation;
+}
